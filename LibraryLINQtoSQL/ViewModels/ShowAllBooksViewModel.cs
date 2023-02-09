fbcf9e8e9bd164939b4e0a00604510e2efdf6d46
ZoreX <aydinhsimli@gmail.com>
2023-02-09 16:16:19 +0400

    Add project files.

---
+++ LibraryLINQtoSQL/ViewModels/ShowAllBooksViewModel.cs
@@ -0,0 +1,29 @@
+﻿using LibraryLINQtoSQL.Commands;
+using LibraryLINQtoSQL.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryLINQtoSQL.ViewModels
+{
+    public class ShowAllBooksViewModel:BaseViewModel
+    {
+        public RelayCommand BackCommand { get; set; }
+
+        private ObservableCollection<Book> books;
+
+        public ObservableCollection<Book> Books
+        {
+            get { return books; }
+            set { books = value; OnPropertyChanged(); }
+        }
+
+        public ShowAllBooksViewModel()
+        {
+            
+        }
+    }
+}
